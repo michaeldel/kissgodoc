b3d0842f8e2417aac9c0d22a56e608ac4c3a5700
michaeldel <michaeldel@protonmail.com>
2021-06-19 13:42:06 +0200

    Add other symbol types

--- main.go
+++ main.go
@@ -22,8 +22,17 @@ func main() {
 		if err != nil {
 			continue // TODO: is it right ?
 		}
+		for _, cn := range docpkg.Consts {
+			fmt.Println(pkg.Name, "const", cn.Names, cn.Decl)
+		}
+		for _, vr := range docpkg.Vars {
+			fmt.Println(pkg.Name, "var", vr.Names, vr.Decl)
+		}
+		for _, tp := range docpkg.Types {
+			fmt.Println(pkg.Name, "type", tp.Name)
+		}
 		for _, fn := range docpkg.Funcs {
-			fmt.Println("  ", fn.Name, fn.Decl)
+			fmt.Println(pkg.Name, "func", fn.Name, fn.Decl)
 		}
 
 	}
